f334d1b7c5637945f0e30c93ccdb1a8502842b9c
Expant <anton532202@gmail.com>
2023-06-06 10:58:25 +0300

    realize welcome function using the readline-sync package

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
+import { welcome } from "../src/cli.js";
+
+welcome();
---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+export const welcome = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}`);
+};
