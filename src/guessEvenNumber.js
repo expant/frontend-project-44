93fb0d46e3d453d284a728c0e53c2d40db67ed54
Expant <anton532202@gmail.com>
2023-06-07 10:30:08 +0300

    refactor definition code the brain-even game

--- src/guessEvenNumber.js
+++ src/guessEvenNumber.js
@@ -1,35 +1,37 @@
 import readlineSync from 'readline-sync';
 import welcome from './cli.js';
 
-const defineNumber = (maxNumber) => {
-  const randomNumber = Math.floor(Math.random() * maxNumber);
-  const isEven = randomNumber % 2 === 0
-    ? 'yes' : 'no';
-
-  return [randomNumber, isEven];
+const getAnswers = (randomNumber, correctAnswer) => {
+  console.log(`Question: ${randomNumber}`);
+  return [
+    readlineSync.question('Your answer: '),
+    correctAnswer,
+  ];
 };
 
-const guessEvenNumber = () => {
+const defineNumber = (maxNumber) => {
+  const randomNumber = Math.floor(Math.random() * maxNumber);
+  return getAnswers(
+    randomNumber,
+    randomNumber % 2 === 0 ? 'yes' : 'no',
+  );
+};
+
+export default () => {
   const name = welcome();
+
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   const maxNumber = 100;
 
   for (let index = 0; index < 3; index += 1) {
-    const [randomNumber, isEven] = defineNumber(maxNumber);
+    const [userAnswer, correctAnswer] = defineNumber(maxNumber);
 
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-
-    if (isEven !== answer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven}'`);
+    if (correctAnswer !== userAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
       console.log(`Let's try again, ${name}`);
       return;
     }
-
     console.log('Correct!');
   }
-
   console.log(`Congratulations, ${name}!`);
 };
-
-export default guessEvenNumber;
