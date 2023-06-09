140daffafe4008164b8358af32c386244d2e8097
Expant <anton532202@gmail.com>
2023-06-09 13:52:01 +0300

    rename function

--- src/games/even.js
+++ src/games/even.js
@@ -5,11 +5,11 @@ const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const getRandomNum = (maxNum) => Math.floor(Math.random() * maxNum);
 const isEven = (num) => num % 2 === 0;
 
-const defineNumbers = (maxNum) => {
+const getGameData = (maxNum) => {
   const randomNum = getRandomNum(maxNum);
   const correctAnswer = isEven(randomNum) ? 'yes' : 'no';
 
   return [randomNum, correctAnswer];
 };
 
-export default () => app(rule, defineNumbers);
+export default () => app(rule, getGameData);
