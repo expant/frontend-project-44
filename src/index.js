0e91e078f87c1ab588cd254f8edb6fe20b178bf2
Expant <anton532202@gmail.com>
2023-06-09 10:53:00 +0300

    realize common logic for games

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+import welcome from './games/cli.js';
+
+const MAX_NUMBER = 100;
+const MAX_ROUNDS_COUNT = 3;
+
+const printRule = (rule) => console.log(rule);
+const printCongratulations = (username) => console.log(`Congratulations, ${username}!`);
+const printLoss = (username, userAnswer, correctAnswer) => {
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+  console.log(`Let's try again, ${username}`);
+};
+
+const askQuestion = (question) => console.log(`Question: ${question}`);
+
+const getUserAnswer = () => readlineSync.question('Your answer: ');
+
+const isCorrectAnswer = (correctAnswer, userAnswer) => correctAnswer === userAnswer;
+
+export default (rule, getGameData) => {
+  const username = welcome();
+  printRule(rule);
+
+  for (let index = 0; index < MAX_ROUNDS_COUNT; index += 1) {
+    const [question, correctAnswer] = getGameData(MAX_NUMBER);
+    askQuestion(question);
+
+    const userAnswer = getUserAnswer();
+
+    if (!isCorrectAnswer(correctAnswer, userAnswer)) {
+      printLoss(username, userAnswer, correctAnswer);
+      return;
+    }
+  }
+  printCongratulations(username);
+};
