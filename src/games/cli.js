675cf5f92c279e6d708019492c9d6225b5fb5749
Expant <anton532202@gmail.com>
2023-06-10 23:30:35 +0300

    refactor welcome

--- src/games/cli.js
+++ src/games/cli.js
@@ -1,9 +1,17 @@
 import readlineSync from 'readline-sync';
 
+const printWelcome = () => console.log('Welcome to the Brain Games!');
+const printHello = (name) => console.log(`Hello, ${name}`);
+
+const getName = () => readlineSync.question('May I have your name? ');
+
 const welcome = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
+  printWelcome();
+
+  const name = getName();
+
+  printHello(name);
+
   return name;
 };
 
