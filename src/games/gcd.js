0fd1002ac6ee6a75ede56af141d40efb8b3a0f00
Expant <anton532202@gmail.com>
2023-06-10 13:04:00 +0300

    refactor tab

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -9,7 +9,7 @@ const getDivider = (num1, num2) => num1 % num2;
 const getGcd = (nums) => {
   let [num1, num2] = nums;
   let divider = getDivider(num1, num2);
-  
+
   while (divider) {
     num1 = num2;
     num2 = divider;
@@ -19,11 +19,11 @@ const getGcd = (nums) => {
 };
 
 const getGameData = (maxNum) => {
-	const num1 = getRandomNum(maxNum);
-	const num2 = getRandomNum(maxNum);
+  const num1 = getRandomNum(maxNum);
+  const num2 = getRandomNum(maxNum);
   const expression = `${num1} ${num2}`;
   const gcd = String(getGcd([num1, num2]));
-	
+
   return [expression, gcd];
 };
 
