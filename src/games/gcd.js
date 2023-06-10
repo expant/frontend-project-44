922c75eda315d5ba436fe6bceeec4a0ec25cb82c
Expant <anton532202@gmail.com>
2023-06-10 12:58:26 +0300

    realize gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,11 +4,27 @@ const rule = 'Find the greatest common divisor of given numbers.';
 
 const getRandomNum = (maxNum) => Math.floor(Math.random() * maxNum);
 
+const getDivider = (num1, num2) => num1 % num2;
+
+const getGcd = (nums) => {
+  let [num1, num2] = nums;
+  let divider = getDivider(num1, num2);
+  
+  while (divider) {
+    num1 = num2;
+    num2 = divider;
+    divider = getDivider(num1, num2);
+  }
+  return num2;
+};
+
 const getGameData = (maxNum) => {
 	const num1 = getRandomNum(maxNum);
 	const num2 = getRandomNum(maxNum);
-
+  const expression = `${num1} ${num2}`;
+  const gcd = String(getGcd([num1, num2]));
 	
+  return [expression, gcd];
 };
 
 export default () => app(rule, getGameData);
