40103a396133c42fc2cd0aacb8d5ef18a3813412
Expant <anton532202@gmail.com>
2023-06-12 20:39:40 +0300

    refactor getRandomNum

--- src/games/even.js
+++ src/games/even.js
@@ -2,7 +2,7 @@ import app from '../index.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const getRandomNum = (maxNum) => Math.floor(Math.random() * maxNum);
+const getRandomNum = (maxNum) => Math.floor(Math.random() * maxNum) + 1;
 const isEven = (num) => num % 2 === 0;
 
 const getGameData = (maxNum) => {
