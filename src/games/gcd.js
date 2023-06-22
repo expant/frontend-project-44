8de0c9f4574a9c821e489c514b8635f46d71fec7
Expant <anton532202@gmail.com>
2023-06-22 11:45:41 +0300

    refactor getGcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,25 +5,20 @@ const RULE = 'Find the greatest common divisor of given numbers.';
 
 const NUM_INTERVAL = [1, 100];
 
-const getDivider = (num1, num2) => num1 % num2;
-
-const getGcd = (nums) => {
-  let [num1, num2] = nums;
-  let divider = getDivider(num1, num2);
-
-  while (divider) {
-    num1 = num2;
-    num2 = divider;
-    divider = getDivider(num1, num2);
+const getGcd = (x, y) => {
+  if (x % y === 0) {
+    return y;
   }
-  return num2;
+
+  return getGcd(y, x % y);
 };
 
 const getGameData = () => {
   const num1 = getRandomNum(...NUM_INTERVAL);
   const num2 = getRandomNum(...NUM_INTERVAL);
+
   const question = `${num1} ${num2}`;
-  const correctAnswer = String(getGcd([num1, num2]));
+  const correctAnswer = String(getGcd(num1, num2));
 
   return [question, correctAnswer];
 };
