2e4990cf5add4b835596853d20a3e0fb69765192
Expant <anton532202@gmail.com>
2023-06-22 09:13:28 +0300

    refactor getting data from user

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import readlineSync from 'readline-sync';
 
 const MAX_ROUNDS_COUNT = 3;
 
-const getUsername = () => readlineSync.question('May I have your name? ');
-const getUserAnswer = () => readlineSync.question('Your answer: ');
-
 const isCorrectAnswer = (correctAnswer, userAnswer) => correctAnswer === userAnswer;
 
 export default (rule, getGameData) => {
   console.log('Welcome to the Brain Games!');
-  const username = getUsername();
+  const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}`);
 
   console.log(rule);
@@ -19,7 +16,7 @@ export default (rule, getGameData) => {
 
     console.log(`Question: ${question}`);
 
-    const userAnswer = getUserAnswer();
+    const userAnswer = readlineSync.question('Your answer: ');
 
     if (!isCorrectAnswer(correctAnswer, userAnswer)) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
