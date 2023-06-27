301d4af6ca89ce50970d536cdc746c3828756b25
Expant <anton532202@gmail.com>
2023-06-27 08:23:44 +0300

    put away getQuestionAndAnswer function from utils.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,5 @@
 import app from '../index.js';
-import { getQuestionAndAnswer } from '../utils.js';
+import { getRandomNum } from '../utils.js';
 
 const RULE = 'Answer "yes" if the number is even, otherwise answer "no".';
 
@@ -7,6 +7,13 @@ const NUM_INTERVAL = [1, 100];
 
 const isEven = (num) => num % 2 === 0;
 
-const getGameData = () => getQuestionAndAnswer(NUM_INTERVAL, isEven);
+const getGameData = () => {
+  const number = getRandomNum(...NUM_INTERVAL);
+
+  const question = String(number);
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
+};
 
 export default () => app(RULE, getGameData);
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,3 @@
 export const getRandomNum = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 export const getRandomIndex = (collection) => Math.floor(Math.random() * collection.length);
-
-export const getQuestionAndAnswer = (NUM_INTERVAL, isNumTo) => {
-  const number = getRandomNum(...NUM_INTERVAL);
-
-  const question = String(number);
-  const correctAnswer = isNumTo(number) ? 'yes' : 'no';
-
-  return [question, correctAnswer];
-};
